6df7aca33955bbbda9a5da0f8792913edcce6414
Adam Kvasnička <adam.kvasnicka.dmd@gmail.com>
2024-07-06 21:41:43 +0200

    chore: listen function doc

--- lib/apple_pencil_double_tap.dart
+++ lib/apple_pencil_double_tap.dart
@@ -9,6 +9,8 @@ export 'package:apple_pencil_double_tap/entities/squeeze_phase.dart';
 export 'package:apple_pencil_double_tap/entities/tap_action.dart';
 
 class ApplePencilDoubleTap {
+  /// Note that when targetting devices with iPadOS <17.5 use [v1Callback] callbacks.
+  /// For iPadOS >=17.5 make sure to implement [onTapAction] or [onSqueeze] callbacks.
   void listen({
     Function(PreferredAction action)? v1Callback,
     Function(TapAction action)? onTapAction,
